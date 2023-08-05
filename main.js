b4b2edce70acbb4db171c46adc242e25300a4366
RicardoBertarini <bertarini@gmail.com>
2023-08-05 09:15:39 -0300

    Base inicial del menu

--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-alert("Hola mi supercreador")
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
 
-function iniciarCariluma(){
-    let botonArquitectura = document.getElementById('boton-Arquitectura')
-    botonArquitectura.addEventListener('click', seleccionarArquitectura)  
+menuEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive');
 }
-
-function seleccionarArquitectura(){
-    let inputLayout = document.getElementById('Layout')
-    let inputFlujograma = document.getElementById('Flujograma')
-    let inputProcedimiento = document.getElementById('Procedimiento')
-    let inputInstructivo = document.getElementById('Instructivo')
-    let inputIndicadores = document.getElementById('Indicadores')
-    let inputReglas = document.getElementById('Reglas')
-   
-    if(inputLayout.checked){
-        alert('Seleccionaste Layout')
-    } else if(inputFlujograma.checked){
-        alert('Seleccionaste a Flujograma')
-    } else if(inputProcedimiento.checked){
-        alert('Seleccionaste a Procedimiento')
-    } else if(inputInstructivo.checked){
-        alert('Seleccionaste a Instructivo')
-    } else if(inputIndicadores.checked){
-        alert('Seleccionaste a Indicadores')
-    } else if(inputReglas.checked){
-        alert('Seleccionaste a Reglas')
-    } else{
-        alert('No seleccionaste una opción')
-    }
-}
-
-window.addEventListener('load', iniciarCariluma)
